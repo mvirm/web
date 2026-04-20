9eb19f084ae35a9fd83a6482fc486b20a81df9a1
Virginia <mvirginiamontoya@yahoo.com.ar>
2026-04-20 12:58:45 -0300

    configuracion inicial tailwind

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import "./index.css";
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center">
       <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400">
-        ¡Tailwind funcionando!
+        ¡Tailwind funcionando correctamente!
       </h1>
     </div>
   );
