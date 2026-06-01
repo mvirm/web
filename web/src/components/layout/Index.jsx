c11e510e82f5d87fe5156e39b9806c3378867ee0
Virginia <mvirginiamontoya@yahoo.com.ar>
2026-06-01 12:52:33 -0300

    cambios en loading inicial para mejorar rendimiento

--- web/src/components/layout/Index.jsx
+++ web/src/components/layout/Index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FormModal from "../form/FormModal.jsx";
 import Hero from "../hero/Hero.jsx";
 import ServicesAndBenefits from "./ServicesAndBenefits.jsx";
@@ -10,30 +10,36 @@ import InitialLoading from "../InitialLoading/InitialLoading.jsx";
 
 const Index = () => {
   const [showModal, setShowModal] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
 
-  // Simula una carga inicial de 5 segundos
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 5500);
+  const [isLoading, setIsLoading] = useState(() => {
+    return !sessionStorage.getItem("visited");
+  });
 
+  useEffect(() => {
+    if (!isLoading) return;
+
+    const timer = setTimeout(() => {
+      sessionStorage.setItem("visited", "true");
+      setIsLoading(false);
+    }, 5500);
+
+    return () => clearTimeout(timer);
+  }, [isLoading]);
   return (
     <main className="min-h-screen max-w-screen gap-0">
-      {isLoading ? (
-        <InitialLoading />
-      ) : (
-        <>
-          <Hero setShowModal={setShowModal} />
-          <ServicesAndBenefits setShowModal={setShowModal} />
-          <ProjectsAndProcesses setShowModal={setShowModal} />
-          <div className="w-full bg-linen flex-1 items-center pb-8 mt-0 md:mt-2 lg:mt-4">
-            <img src={isotype} alt="isotipo" className="w-full" />
-          </div>
-          <PhilosophyAndFinallyCta setShowModal={setShowModal} />
-          <Footer setShowModal={setShowModal} />
-        </>
-      )}
+      <>
+        <Hero setShowModal={setShowModal} />
+        <ServicesAndBenefits setShowModal={setShowModal} />
+        <ProjectsAndProcesses setShowModal={setShowModal} />
+        <div className="w-full bg-linen flex-1 items-center pb-8 mt-0 md:mt-2 lg:mt-4">
+          <img src={isotype} alt="isotipo" className="w-full" />
+        </div>
+        <PhilosophyAndFinallyCta setShowModal={setShowModal} />
+        <Footer setShowModal={setShowModal} />
+      </>
+      {/* )} */}
       {showModal && <FormModal setShowModal={setShowModal} />}
+      {isLoading && <InitialLoading />}
     </main>
   );
 };
